2aa51d478cf7a5f22c874c88c0fe4349f21ae579
osvaldomoreirajunior <38547771+osvaldomoreirajunior@users.noreply.github.com>
2018-04-22 16:57:15 -0300

    Update QuartoDesafio.py

--- QuartoDesafio.py
+++ QuartoDesafio.py
@@ -3,22 +3,20 @@ import string
 a = list(string.ascii_lowercase)
 b = list(range(1, 27))
 c = list(string.ascii_uppercase)
-d = list(range(27, 52))
+d = list(range(27, 53))
 
 palavra = str(input('Digite uma palavra: '))
 
 def test(entrada):
-    encontrado = [b[a.index(s)] for s in entrada]
-    return sum(encontrado)
-def test2 (entrada2):
-    encontrado = [d[c.index(x)] for x in entrada2]
+    if palavra == palavra.lower():
+        encontrado = [b[a.index(s)] for s in entrada]
+    elif palavra == palavra.upper():
+        encontrado = [d[c.index(s)] for s in entrada]
     return sum(encontrado)
 
-if palavra == palavra.lower():
-     resultado_palavra = test(palavra)
-elif palavra == palavra.upper():
-    resultado_palavra = test2(palavra)
+resultado_palavra = test(palavra)
 print(resultado_palavra)
+
 if resultado_palavra % 2 ==1:
     print(f'A palavra {palavra} é PRIMA')
 else:
