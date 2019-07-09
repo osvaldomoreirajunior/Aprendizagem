ae44353ef9d98c4bc30d930954623d388ebefeb6
Júnior DEV <Jnior DEV@DESKTOP-T19IHG3>
2019-07-08 23:38:00 -0300

    finanças com spring

---
+++ src/main/java/com/example/finan/as/FinançasApplication.java
@@ -0,0 +1,13 @@
+package com.example.finan.as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinançasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinançasApplication.class, args);
+	}
+
+}
